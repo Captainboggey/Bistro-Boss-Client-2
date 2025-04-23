8477faf9cd22801291442b9da8268cfb90fd5f7d
Captainboggey <anik35-700@diu.edu.bd>
2025-04-23 17:37:43 +0600

    testimonials added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -5,9 +5,9 @@ import  './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item pt-10 my-20 text-white'>
+        <div className='featured-item bg-fixed pt-10 my-20 text-white'>
             <SectionTitle heading={"Featured items"} subHeading={"check it out"}> </SectionTitle>
-                <div className='md:flex justify-center items-center pb-20 pt-12 px-36 '>
+                <div className='md:flex justify-center items-center pb-20 pt-12 px-36 bg-slate-800 opacity-60 '>
                     <div>
                         <img src={fimg} alt="" />
                     </div>
@@ -17,7 +17,7 @@ const Featured = () => {
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae adipisci, deleniti cum quae in molestias exercitationem nulla, laudantium laboriosam sed maiores deserunt rem modi. Fuga laboriosam repellendus aut atque accusantium reiciendis delectus, placeat aliquid corrupti nihil amet. Rem ullam iure vel ad praesentium, totam vero nisi. Eius dolorum eligendi ducimus.
                         </p>
-                        <button className='btn btn-outline'>Order Now</button>
+                        <button className='btn btn-outline border-0 border-b-2 mt-2'>Order Now</button>
                     </div>
                 </div>
            
