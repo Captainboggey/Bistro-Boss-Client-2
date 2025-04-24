fdfaf86c2bd5694c85fb91891f1b8022f9f311d4
Captainboggey <anik35-700@diu.edu.bd>
2025-04-24 14:29:42 +0600

    menu page added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menubg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>This is Menu</h2>
+           <Cover coverTitle={'our menu'} img={menubg}></Cover>
+           <PopularMenu></PopularMenu>
         </div>
     );
 };
