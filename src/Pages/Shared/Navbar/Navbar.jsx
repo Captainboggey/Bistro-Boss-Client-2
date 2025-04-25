b262c50b97d66d71d136130f9203b9153a5e6c81
Captainboggey <anik35-700@diu.edu.bd>
2025-04-25 16:11:54 +0600

    adding captcha

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <li><Link to={'/'}>Home</Link></li>
     <li><Link to={'/menu'}>Our Menu</Link></li>
     <li><Link to={'/order/salad'}>Order Food</Link></li>
+    <li><Link to={'/login'}>Login</Link></li>
     </>
     return (
         <div className="navbar bg-black max-w-screen-xl mx-auto text-white bg-opacity-30 pb-2 fixed z-10">
