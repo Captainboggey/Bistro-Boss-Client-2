87b2fa888c82a705d4a40af48d982620f63b6e35
Captainboggey <anik35-700@diu.edu.bd>
2025-04-26 16:55:03 +0600

    table added

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaAd, FaCalendar, FaHome, FaList } from 'react-icons/fa';
+import { FaAd, FaCalendar, FaHome, FaList, FaSearch } from 'react-icons/fa';
 import { HiMiniShoppingCart } from 'react-icons/hi2';
 import { NavLink, Outlet } from 'react-router-dom';
 
@@ -7,32 +7,42 @@ const DashBoard = () => {
     return (
         <div className='flex'>
             <div className="w-64 min-h-screen bg-orange-400">
-                <ul className='menu p-4'>
+                <ul className='menu p-4 '>
                 <li>
                     
-                    <NavLink to={'/dashboard/userHome'}><FaHome></FaHome> User Home</NavLink>
+                    <NavLink className='hover:bg-purple-500 selection:bg-purple-600' to={'/dashboard/userHome'}><FaHome></FaHome> User Home</NavLink>
                     </li>
                     <li>
                     
-                    <NavLink to={'/dashboard/reservation'}><FaCalendar></FaCalendar> Reservation</NavLink>
+                    <NavLink className='hover:bg-purple-500 selection:bg-purple-600' to={'/dashboard/reservation'}><FaCalendar></FaCalendar> Reservation</NavLink>
                     </li>
                     <li>
                     
-                        <NavLink to={'/dashboard/cart'}><HiMiniShoppingCart /> My Cart</NavLink>
+                        <NavLink className='hover:bg-purple-500 ' to={'/dashboard/cart'}><HiMiniShoppingCart /> My Cart</NavLink>
                         </li>
                         <li>
                     
-                    <NavLink to={'/dashboard/review'}><FaAd></FaAd>Review</NavLink>
+                    <NavLink className='hover:bg-purple-500 selection:bg-purple-600' to={'/dashboard/review'}><FaAd></FaAd>Review</NavLink>
                     </li>
                     <li>
                     
-                    <NavLink to={'/dashboard/bookings'}><FaList></FaList> User Home</NavLink>
+                    <NavLink className='hover:bg-purple-500 selection:bg-purple-600' to={'/dashboard/bookings'}><FaList></FaList> User Home</NavLink>
                     </li>
+                    <div className='divider divider-primary w-full'></div>
+                    <li>
+                    
+                    <NavLink className='hover:bg-purple-500 selection:bg-purple-600' to={'/'}><FaHome></FaHome>  Home</NavLink>
+                    </li>
+                    <li>
+                    
+                    <NavLink className='hover:bg-purple-500 selection:bg-purple-600' to={'/menu'}><FaSearch></FaSearch>Menu</NavLink>
+                    </li>
+                    
                    
                 </ul>
             </div>
 
-            <div className='flex-1'>
+            <div className='flex-1 p-8'>
                 <Outlet></Outlet>
             </div>
         </div>
