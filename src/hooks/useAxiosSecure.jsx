61025d74dfad0ee8685819a8cfe62b2b15d14ece
Captainboggey <anik35-700@diu.edu.bd>
2025-04-27 18:19:20 +0600

    axios secure implemneted

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,28 @@
 import axios from "axios";
 
- const axiosSecure = axios.create({
-    baseURL:'http://localhost:5000'
+const axiosSecure = axios.create({
+   baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
+   axiosSecure.interceptors.request.use(function (config) {
+      const token = localStorage.getItem('access-token')
+
+      console.log('request hit by interceptor', token)
+      config.headers.authorization = `Bearer ${token}`
+      return config
+   }, function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+   })
+
+   axiosSecure.interceptors.response.use(function (response) {
+      return response
+   }, (error) => {
+      const status = error.response.status
+      console.log('status error in the interceptor',status)
+      return Promise.reject(error)
+   })
    return axiosSecure;
 };
 
