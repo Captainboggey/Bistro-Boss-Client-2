b597e4b56bdc1c51576b10f79226c8cf7427acac
Captainboggey <anik35-700@diu.edu.bd>
2025-04-29 22:59:30 +0600

    delete added

--- src/Pages/Dashboard/MangeItems/MangeItems.jsx
+++ src/Pages/Dashboard/MangeItems/MangeItems.jsx
@@ -3,9 +3,46 @@ import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import useMenu from '../../../hooks/useMenu';
 import { FaEdit, FaTrash, FaUser } from 'react-icons/fa';
 import { FaUpLong } from 'react-icons/fa6';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const MangeItems = () => {
-    const [menu] = useMenu()
+    const [menu,loading,refetch] = useMenu()
+    const axiosSecure = useAxiosSecure();
+    const handleDeleteItem=(item)=>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+                const res =await axiosSecure.delete(`/menu/${item._id}`);
+                console.log(res.data,item._id,item.name)
+                if(res.data.deletedCount>0){
+                    refetch()
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: "top-end",
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                          toast.onmouseenter = Swal.stopTimer;
+                          toast.onmouseleave = Swal.resumeTimer;
+                        }
+                      });
+                      Toast.fire({
+                        icon: "success",
+                        title: `${item.name}Deleted Successfully`
+                      });
+                }
+            }
+          });
+    }
     return (
         <div>
             <SectionTitle heading={'Manage All items'} subHeading={'Hurry Up'}></SectionTitle>
@@ -54,7 +91,7 @@ const MangeItems = () => {
                                         <button  className="btn btn-ghost btn-lg"><FaEdit className='text-red-600'></FaEdit></button>
                                     </td>
                                     <td>
-                                        <button onClick={()=>handleDeleteItem(item._id)} className="btn btn-ghost btn-lg"><FaTrash className='text-red-600'></FaTrash></button>
+                                        <button onClick={()=>handleDeleteItem(item)} className="btn btn-ghost btn-lg"><FaTrash className='text-red-600'></FaTrash></button>
                                     </td>
                                 </tr>
                                )
