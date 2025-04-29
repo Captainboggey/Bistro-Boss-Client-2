aa83a8622bbcb5f4ddddc4f4c69a2c70e9132ea5
Captainboggey <anik35-700@diu.edu.bd>
2025-04-29 23:24:00 +0600

    update deployed

--- src/Pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/Pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -36,8 +36,8 @@ const UpdateItem = () => {
                 }
                 const menuRes = await axiosSecure.patch(`/menu/${_id}`,menuItem)
                 console.log(menuRes.data)
-                if(menuRes.data.insertedId){
-                    reset()
+                if(menuRes.data.modifiedCount>0){
+                    // reset()
                     const Toast = Swal.mixin({
                         toast: true,
                         position: "top-end",
